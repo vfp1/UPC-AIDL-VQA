be8ce47dfcc9f7366ae3c0e144a60c7de14b8f74
Victor Pajuelo Madrigal <victor.fpm@gmail.com>
2019-06-22 11:45:15 +0200

    Revert "Develop"

--- upc-vqa/training.py
+++ upc-vqa/training.py
@@ -9,6 +9,15 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 import train
 
+__author__ = "Victor Pajuelo Madrigal"
+__copyright__ = "Copyright 2019, UPC Group"
+__credits__ = ["Victor Pajuelo Madrigal", "Jiasen Lu", "@abhshkdz"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Victor Pajuelo Madrigal"
+__email__ = "-"
+__status__ = "Development"
+
 # Preprocess data
 
 path = "G:/My Drive/Studies/UPC-AIDL/VQA/data/"
