588187189fa2bf44eaada28d6b89d3606d490743
vfp1 <victor.fpm@gmail.com>
2019-06-30 13:00:17 +0200

    Away with merges

--- upc-vqa/training.py
+++ upc-vqa/training.py
@@ -31,7 +31,7 @@ try:
                             keras_metrics='categorical_accuracy', learning_rate=1e-2,
                             optimizer='rmsprop', fine_tuned=True, test_size=0.20, vgg_frozen=4,
                             lstm_hidden_nodes=512, lstm_num_layers=3, fc_hidden_nodes=1024, fc_num_layers=3,
-                            merge_method='multiply')
+                            merge_method='concatenate')
 
 except:
 
@@ -43,4 +43,4 @@ except:
                             keras_metrics='categorical_accuracy', learning_rate=1e-4,
                             optimizer='rmsprop', fine_tuned=True, test_size=0.20, vgg_frozen=4,
                             lstm_hidden_nodes=512, lstm_num_layers=6, fc_hidden_nodes=1024, fc_num_layers=6,
-                            merge_method='multiply')
+                            merge_method='concatenate')
